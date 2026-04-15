d4c44f0f58ce11781aed45ffb3cf8aa44440ee4f
nicolas.bernal <nicolas.bernal@ext.experienceis.es>
2026-04-15 15:26:44 +0200

    Adsense

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://informacioncantillana.netlify.app',
   output: 'server',
   adapter: netlify(),
   vite: {
